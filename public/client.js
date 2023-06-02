d25bde0951b2535c4db4cb610a86b1f2b554590d
dnloster <ducgt.ng@gmail.com>
2023-06-02 16:00:15 +0700

    update

--- public/client.js
+++ public/client.js
@@ -101,6 +101,9 @@ socket.on("webrtc_offer", async (event) => {
     const remotePeerId = event.senderId;
 
     peerConnections[remotePeerId] = new RTCPeerConnection(iceServers);
+    disconnectButton.addEventListener("click", () => {
+        peerConnections.close();
+    });
     console.log(new RTCSessionDescription(event.sdp));
     peerConnections[remotePeerId].setRemoteDescription(
         new RTCSessionDescription(event.sdp)
@@ -169,8 +172,9 @@ function showVideoConference() {
     roomSelectionContainer.style = "display: none";
     document.getElementsByTagName("BODY")[0].style = "background-color: #eee";
     videoChatContainer.style = "display: block";
+    document.getElementById("controls-container").style =
+        "display: flex; gap: 30px; position: absolute; bottom: 0; left: 50%; margin-bottom: 20px; transform: translateX(-50%)";
     // const localName = document.createElement("label");
-
     // localName.innerText = displayName;
     // localChat.appendChild(localName);
 }
@@ -187,6 +191,87 @@ async function setLocalStream(mediaConstraints) {
     localStream = stream;
     localVideoComponent.srcObject = stream;
     localVideoComponent.style = "box-shadow: rgb(4 4 4 / 94%) 0px 7px 29px 0px";
+
+    // Bật hoặc tắt hình ảnh
+    function toggleVideo() {
+        const videoTracks = stream.getVideoTracks();
+        videoTracks.forEach((track) => {
+            track.enabled = !track.enabled; // Đảo ngược trạng thái bật/tắt
+        });
+    }
+
+    // Bật tắt âm thanh
+    function toggleAudio() {
+        const audioTracks = stream.getAudioTracks();
+        audioTracks.forEach((track) => {
+            track.enabled = !track.enabled;
+        });
+    }
+
+    // Hiển thị trạng thái hình ảnh trong giao diện người dùng
+    function updateVideoStatus() {
+        const videoTracks = stream.getVideoTracks();
+        if (videoTracks.every((track) => !track.enabled)) {
+            document.getElementById(
+                "toggle-video-button"
+            ).innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-video-off" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                <path d="M3 3l18 18"></path>
+                                <path d="M15 11v-1l4.553 -2.276a1 1 0 0 1 1.447 .894v6.764a1 1 0 0 1 -.675 .946"></path>
+                                <path d="M10 6h3a2 2 0 0 1 2 2v3m0 4v1a2 2 0 0 1 -2 2h-8a2 2 0 0 1 -2 -2v-8a2 2 0 0 1 2 -2h1"></path>
+                            </svg>`;
+        } else {
+            document.getElementById(
+                "toggle-video-button"
+            ).innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-video" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                <path d="M15 10l4.553 -2.276a1 1 0 0 1 1.447 .894v6.764a1 1 0 0 1 -1.447 .894l-4.553 -2.276v-4z"></path>
+                                <path d="M3 6m0 2a2 2 0 0 1 2 -2h8a2 2 0 0 1 2 2v8a2 2 0 0 1 -2 2h-8a2 2 0 0 1 -2 -2z"></path>
+                            </svg>`;
+        }
+    }
+
+    // Hiển thị trạng thái hình ảnh trong giao diện người dùng
+    function updateAudioStatus() {
+        const audioTracks = stream.getAudioTracks();
+        if (audioTracks.every((track) => !track.enabled)) {
+            // videoStatus.textContent = "Hình ảnh đã tắt";
+            document.getElementById(
+                "toggle-audio-button"
+            ).innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-microphone-off" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                <path d="M3 3l18 18"></path>
+                                <path d="M9 5a3 3 0 0 1 6 0v5a3 3 0 0 1 -.13 .874m-2 2a3 3 0 0 1 -3.87 -2.872v-1"></path>
+                                <path d="M5 10a7 7 0 0 0 10.846 5.85m2 -2a6.967 6.967 0 0 0 1.152 -3.85"></path>
+                                <path d="M8 21l8 0"></path>
+                                <path d="M12 17l0 4"></path>
+                            </svg>`;
+        } else {
+            document.getElementById(
+                "toggle-audio-button"
+            ).innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-microphone" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                <path d="M9 2m0 3a3 3 0 0 1 3 -3h0a3 3 0 0 1 3 3v5a3 3 0 0 1 -3 3h0a3 3 0 0 1 -3 -3z"></path>
+                                <path d="M5 10a7 7 0 0 0 14 0"></path>
+                                <path d="M8 21l8 0"></path>
+                                <path d="M12 17l0 4"></path>
+                            </svg>`;
+        }
+    }
+
+    // Khi người dùng nhấp vào nút/tắt hình ảnh
+    const toggleVideoButton = document.getElementById("toggle-video-button");
+    toggleVideoButton.addEventListener("click", () => {
+        toggleVideo();
+        updateVideoStatus();
+    });
+
+    // Khi người dùng nhấp vào nút/tắt âm thanh
+    const toggleAudioButton = document.getElementById("toggle-audio-button");
+    toggleAudioButton.addEventListener("click", () => {
+        toggleAudio();
+        updateAudioStatus();
+    });
 }
 
 function addLocalTracks(rtcPeerConnection) {
@@ -290,13 +375,20 @@ function toast({ title = "", type = "info", duration = 3000 }) {
 
         toast.innerHTML = `
                     <div class="toast__icon">
-                        <i class="fa-solid fa-circle-info"></i>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-info-circle-filled" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <path d="M12 2c5.523 0 10 4.477 10 10a10 10 0 0 1 -19.995 .324l-.005 -.324l.004 -.28c.148 -5.393 4.566 -9.72 9.996 -9.72zm0 9h-1l-.117 .007a1 1 0 0 0 0 1.986l.117 .007v3l.007 .117a1 1 0 0 0 .876 .876l.117 .007h1l.117 -.007a1 1 0 0 0 .876 -.876l.007 -.117l-.007 -.117a1 1 0 0 0 -.764 -.857l-.112 -.02l-.117 -.006v-3l-.007 -.117a1 1 0 0 0 -.876 -.876l-.117 -.007zm.01 -3l-.127 .007a1 1 0 0 0 0 1.986l.117 .007l.127 -.007a1 1 0 0 0 0 -1.986l-.117 -.007z" stroke-width="0" fill="currentColor"></path>
+                        </svg>
                     </div>
                     <div class="toast__body">
                         <h3 class="toast__title">${title}</h3>
                     </div>
                     <div class="toast__close">
-                        <i class="fa-solid fa-xmark"></i>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-x" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <path d="M18 6l-12 12"></path>
+                            <path d="M6 6l12 12"></path>
+                        </svg>
                     </div>
                 `;
         main.appendChild(toast);
@@ -305,7 +397,7 @@ function toast({ title = "", type = "info", duration = 3000 }) {
 
 function showErrorToast() {
     toast({
-        title: `Người dùng đã ngắt kết nối`,
+        title: `Ngắt kết nối`,
         type: "info",
         duration: 5000,
     });
